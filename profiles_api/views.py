e64618149c19be9b05b36dacf0a9d470ac1f5c33
Rishabh Khanna <rishabhkhanna726@gmail.com>
2020-09-22 17:16:42 +0530

    first basic APIView - http get function

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class HelloApi(APIView):
+
+    def get(self, request, format=None):
+        """ returns a list of APIView features """
+        
+        an_api = [
+            'API','Django','REST'
+        ]
+
+        return Response({'message':'Hello!','an_api':an_api})
