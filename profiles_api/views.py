fbc4d79c76211166a582033304a72faea40d2d3f
Rishabh Khanna <rishabhkhanna726@gmail.com>
2020-09-23 00:32:41 +0530

    permissions, authorization in views and search filters

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,10 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, filters
 from profiles_api import serializers, models
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from profiles_api import permissions
 
 
 class HelloApi(APIView):
@@ -98,4 +100,8 @@ class UserProfileModelViewSet(viewsets.ModelViewSet):
 
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateUserOWnProfile,)
+    filter_backends =   (filters.SearchFilter,)
+    search_fields = ('name','email',)
 
