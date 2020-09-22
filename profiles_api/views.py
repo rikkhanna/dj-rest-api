1326ec67b3e3e62eb493dca8237a894f3e86f5af
Rishabh Khanna <rishabhkhanna726@gmail.com>
2020-09-22 22:04:31 +0530

    added viewset type API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from profiles_api import serializers
+from rest_framework import viewsets
 
 
 class HelloApi(APIView):
@@ -47,4 +48,46 @@ class HelloApi(APIView):
     def delete(self, request, pk=None):
         """Handle deleting an object"""
         return Response({'method':'DELETE'})
+
+
+class HelloViewSets(viewsets.ViewSet):
+
+    serializer_class = serializers.HelloSerializer
+
+    def list(self,request):
+
+        a_view = [ 'some text', 'some more text']
         
+        return Response({'message':'list','a_view':a_view})
+    
+
+    def create(self,request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+
+            name = serializer.validated_data.get('name')
+            message = f'hello {name}'
+            return Response({'message': message})
+        else:
+
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def retrieve(self, request, pk=None):
+        """getting an object by its ID"""
+        return Response({'http_method':'GET'})
+
+    def update(self, request, pk=None):
+        """updating an object by its ID"""
+        return Response({'http_method':'PUT'})  
+    
+    def partial_update(self, request, pk=None):
+        """partially updating an object by its ID"""
+        return Response({'http_method':'PATCH'})
+
+    def delete(self, request, pk=None):
+        """deleting an object by its ID"""
+        return Response({'http_method':'DELETE'})
